47d95d25d424c6188ff33b60a96e283647fd88b3
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2023-11-03 00:31:27 +0900

    jdbc v1 update

---
+++ jdbc/src/test/java/com/example/demo/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,56 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static com.example.demo.connection.ConnectionConst.*;
+
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach(){
+        //기본 DriverManager - 항상 새로운 커넥션을 획득
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV4", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember =", findMember);
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        //update
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        Assertions.assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
+    }
+
+}
